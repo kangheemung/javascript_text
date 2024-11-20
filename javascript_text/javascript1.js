fb0d242803293a9c097c86fdc6aeb4179ad763fc
kurotamashi30 <kurotamashi@hotmail.co.kr>
2024-11-21 08:26:09 +0900

    add

--- javascript_text/javascript1.js
+++ javascript_text/javascript1.js
@@ -1,12 +1,12 @@
-let paragraph = document.getElementsByTagName("p");
+let paragraph = document.getElementsByTagName('p');
 
-paragraph.addEventListener("click", function() {
-     let el = document.querySelector("p");
-    el.textContent= "clickされました";
+paragraph.addEventListener('click', function () {
+    let el = document.querySelector('p');
+    el.textContent = 'clickされました';
 });
 
 //模範解答
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
     if (event.key === 'y') {
         let p = document.createElement('p');
         p.textContent = '「y」キーが入力されました。';
@@ -17,10 +17,22 @@ document.addEventListener('keydown', function(event) {
 let input = document.getElementsByTagName('input');
 
 for (let i = 0; i < input.length; i++) {
-    input[i].addEventListener('focus', function() {
+    input[i].addEventListener('focus', function () {
         this.style['background-color'] = 'purple';
     });
-    input[i].addEventListener('blur', function() {
+    input[i].addEventListener('blur', function () {
         this.style['background-color'] = 'white';
     });
 }
+
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener('click', function () {
+    let p = document.createElement('p');
+    p.textContent = '追加されました';
+
+    p.addEventListener('mouseover', function () {
+        p.textContent = 'カーソルが重なりました';
+    });
+
+    document.getElementById('result').appendChild(p);
+});
