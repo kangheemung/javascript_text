8246ebc846806e966cf2e9ab4535a8929933a90c
kurotamashi30 <kurotamashi@hotmail.co.kr>
2024-11-25 08:28:18 +0900

    test

--- javascript_text/test3.js
+++ javascript_text/test3.js
@@ -1,10 +1,15 @@
-$funtion(){
+$(function () {
+    $('h1');
+    $('#header').text();
+    console.log($('h1'));
+    console.log($('header'));
+    $('#ele1').cheildren();
+    console.log('#ele1'.children());
+    console.log(typeof $('#ele1').children());
+    console.log($('#ele1').children()[0]);
+    console.log(document.getElementById('ele1'));
 
-    $("h1");
-    $("#header").text();
-    console.log($("h1"));
-    console.log($("header"));
-    $(("#header"));
+    $('#header');
 
-    $("ele1").children();
-}
+    $('ele1').children();
+});
