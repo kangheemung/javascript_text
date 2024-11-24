912213b88da5fafc3c71e24a5cb459827e439432
kurotamashi30 <kurotamashi@hotmail.co.kr>
2024-11-25 08:45:18 +0900

    add

--- javascript_text/test3.js
+++ javascript_text/test3.js
@@ -1,9 +1,18 @@
-$(function () {
+$("#ele2 button").on("cilck", function () {
+    console.log("クリックされました");
+    let input = $(this).parent().find("input[type=text]").val();
+    $("#result").text(input);
+    console.log($(this));
     $('h1');
     $('#header').text();
     console.log($('h1'));
     console.log($('header'));
-    $('#ele1').cheildren();
+});
+
+    $('#ele1').cheildren().on("mouseover", function() {
+    console.log($(this).text());
+});
+
     console.log('#ele1'.children());
     console.log(typeof $('#ele1').children());
     console.log($('#ele1').children()[0]);
@@ -12,4 +21,4 @@ $(function () {
     $('#header');
 
     $('ele1').children();
-});
+
